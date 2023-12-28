88a743c29034501fd3de3ee607a4b45331788895
Mattia Benevento <beneventomattia@gmail.com>
2023-12-28 20:03:56 +0100

    aaa

--- src/it/polito/med/Doctor.java
+++ src/it/polito/med/Doctor.java
@@ -39,7 +39,7 @@ public class Doctor {
 	public void setSpeciality(String speciality) {
 		this.speciality = speciality;
 	}
-	//sc
+	//scrrds
 	// constructorfdgd
 	public Doctor(String id, String name, String surname, String speciality) {
 		super();
